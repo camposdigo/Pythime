7d1ba5bfe37d04d7af401232050892d43e67424c
Rodrigo Campos <rodrigocordeirocampos@gmail.com>
2026-09-05 01:25:34 -0300

    feat: redesign player with rounded cartoon proportions

--- Assets/Scripts/Player/PlayerVisual.cs
+++ Assets/Scripts/Player/PlayerVisual.cs
@@ -101,11 +101,10 @@ namespace Pythime
                 else facing = input.y > 0f ? Direction.Up : Direction.Down;
             }
 
-            var actuallyMoving = movingGrace > 0f;
-            if (actuallyMoving)
+            if (movingGrace > 0f)
             {
                 walkClock += Time.deltaTime;
-                if (walkClock >= 0.12f)
+                if (walkClock >= 0.11f)
                 {
                     walkClock = 0f;
                     walkFrame = (walkFrame + 1) % 4;
@@ -162,7 +161,7 @@ namespace Pythime
             ApplySprite(movingGrace > 0f);
         }
 
-        private void ApplySprite(bool moving = false)
+        private void ApplySprite(bool moving)
         {
             if (spriteRenderer == null) return;
             var key = facing + "_" + walkFrame + "_" + skinIndex + "_" + hairStyleIndex + "_" + hairColorIndex + "_" + shirtIndex + "_" + jacketIndex + "_" + pantsIndex + "_" + shoesIndex + "_" + accessoryIndex;
@@ -172,128 +171,184 @@ namespace Pythime
                 sprite = BuildSprite(facing, walkFrame);
                 cache[key] = sprite;
             }
+
             spriteRenderer.sprite = sprite;
-            spriteRenderer.transform.localPosition = new Vector3(0f, moving && (walkFrame == 1 || walkFrame == 3) ? 0.03f : 0f, 0f);
+            var bounce = moving && (walkFrame == 1 || walkFrame == 3) ? 0.035f : 0f;
+            spriteRenderer.transform.localPosition = new Vector3(0f, bounce, 0f);
         }
 
         private Sprite BuildSprite(Direction direction, int frame)
         {
-            const int width = 18;
-            const int height = 26;
+            const int width = 22;
+            const int height = 30;
             var texture = new Texture2D(width, height, TextureFormat.RGBA32, false)
             {
                 filterMode = FilterMode.Point,
                 wrapMode = TextureWrapMode.Clamp,
-                name = "PythimeAvatar"
+                name = "PythimeAvatarRounded"
             };
             Clear(texture);
 
             var outline = new Color32(24, 27, 31, 255);
             var skin = Skins[skinIndex];
-            var skinDark = Darken(skin, 0.18f);
-            var hair = HairColors[hairColorIndex];
+            var skinDark = Darken(skin, 0.16f);
             var shirt = Shirts[shirtIndex];
             var pants = Pants[pantsIndex];
-            var shoe = shoesIndex == 0 ? new Color32(231, 231, 225, 255) : shoesIndex == 1 ? new Color32(34, 37, 43, 255) : shoesIndex == 2 ? new Color32(91, 63, 43, 255) : new Color32(180, 53, 52, 255);
+            var hair = HairColors[hairColorIndex];
+            var shoe = ShoeColor();
 
-            var legShift = frame == 1 ? 1 : frame == 3 ? -1 : 0;
-            DrawLeg(texture, 4, 1 + Mathf.Max(0, legShift), pants, shoe, outline);
-            DrawLeg(texture, 10, 1 + Mathf.Max(0, -legShift), pants, shoe, outline);
+            var leftStep = frame == 1 ? 1 : frame == 3 ? -1 : 0;
+            var rightStep = -leftStep;
+            DrawLeg(texture, 6, 2 + Mathf.Max(0, leftStep), pants, shoe, outline);
+            DrawLeg(texture, 12, 2 + Mathf.Max(0, rightStep), pants, shoe, outline);
 
-            FillRect(texture, 4, 7, 10, 8, outline);
-            FillRect(texture, 5, 8, 8, 6, shirt);
-            DrawJacket(texture, jacketIndex, outline);
+            DrawTorso(texture, shirt, outline);
+            DrawJacket(texture, outline);
 
-            var swing = frame == 1 ? 1 : frame == 3 ? -1 : 0;
-            FillRect(texture, 2, 8 + swing, 3, 7, outline);
-            FillRect(texture, 3, 9 + swing, 2, 5, skin);
-            FillRect(texture, 13, 8 - swing, 3, 7, outline);
-            FillRect(texture, 13, 9 - swing, 2, 5, skinDark);
+            var armSwing = frame == 1 ? 1 : frame == 3 ? -1 : 0;
+            DrawArm(texture, 3, 10 + armSwing, skin, outline);
+            DrawArm(texture, 16, 10 - armSwing, skinDark, outline);
 
-            FillRect(texture, 4, 14, 10, 10, outline);
-            FillRect(texture, 5, 15, 8, 8, skin);
-            FillRect(texture, 5, 15, 2, 8, skinDark);
+            FillEllipse(texture, 11, 22, 8, 7, outline);
+            FillEllipse(texture, 11, 22, 7, 6, skin);
+            if (direction == Direction.Left) FillRect(texture, 4, 20, 2, 4, skinDark);
+            if (direction == Direction.Right) FillRect(texture, 16, 20, 2, 4, skin);
+
             DrawHair(texture, direction, hair, outline);
             DrawFace(texture, direction, outline);
             DrawAccessory(texture, direction, outline);
 
             texture.Apply(false, false);
-            var sprite = Sprite.Create(texture, new Rect(0, 0, width, height), new Vector2(0.5f, 0.08f), 16f);
-            sprite.name = keyName();
+            var sprite = Sprite.Create(texture, new Rect(0, 0, width, height), new Vector2(0.5f, 0.07f), 16f);
+            sprite.name = "Avatar_" + presetIndex + "_" + jacketIndex + "_" + hairStyleIndex;
             return sprite;
         }
 
-        private string keyName()
+        private void DrawTorso(Texture2D t, Color32 shirt, Color32 outline)
         {
-            return "Avatar_" + presetIndex + "_" + jacketIndex + "_" + hairStyleIndex;
+            FillRect(t, 6, 8, 10, 8, outline);
+            FillRect(t, 5, 10, 12, 4, outline);
+            FillRect(t, 7, 9, 8, 6, shirt);
+            FillRect(t, 6, 11, 10, 3, shirt);
+            FillRect(t, 8, 15, 6, 2, outline);
+            FillRect(t, 9, 15, 4, 1, shirt);
         }
 
         private void DrawLeg(Texture2D t, int x, int y, Color32 pants, Color32 shoe, Color32 outline)
         {
-            FillRect(t, x, y, 4, 7, outline);
-            FillRect(t, x + 1, y + 2, 2, 4, pants);
-            FillRect(t, x, y, 4, 2, shoe);
+            FillRect(t, x, y + 2, 4, 6, outline);
+            FillRect(t, x + 1, y + 3, 2, 4, pants);
+            FillRect(t, x - 1, y, 5, 3, outline);
+            FillRect(t, x, y + 1, 4, 1, shoe);
+            t.SetPixel(x + 3, y, shoe);
         }
 
-        private void DrawJacket(Texture2D t, int style, Color32 outline)
+        private void DrawArm(Texture2D t, int x, int y, Color32 skin, Color32 outline)
         {
-            if (style == 0) return;
+            FillRect(t, x, y, 3, 6, outline);
+            FillRect(t, x + 1, y + 1, 2, 4, skin);
+            FillEllipse(t, x + 1, y, 2, 2, outline);
+            t.SetPixel(x + 1, y, skin);
+        }
+
+        private void DrawJacket(Texture2D t, Color32 outline)
+        {
+            if (jacketIndex == 0) return;
 
             Color32 jacket;
-            if (style == 1) jacket = new Color32(183, 54, 52, 255);
-            else if (style == 2) jacket = new Color32(67, 102, 150, 255);
-            else if (style == 3) jacket = new Color32(43, 45, 51, 255);
-            else if (style == 4) jacket = new Color32(226, 229, 225, 255);
+            if (jacketIndex == 1) jacket = new Color32(183, 54, 52, 255);
+            else if (jacketIndex == 2) jacket = new Color32(67, 102, 150, 255);
+            else if (jacketIndex == 3) jacket = new Color32(43, 45, 51, 255);
+            else if (jacketIndex == 4) jacket = new Color32(226, 229, 225, 255);
             else jacket = new Color32(68, 62, 113, 255);
 
-            FillRect(t, 4, 7, 10, 8, outline);
-            FillRect(t, 5, 8, 8, 6, jacket);
-            FillRect(t, 8, 8, 2, 6, Shirts[shirtIndex]);
+            FillRect(t, 6, 8, 10, 8, outline);
+            FillRect(t, 7, 9, 8, 6, jacket);
+            FillRect(t, 10, 9, 2, 6, Shirts[shirtIndex]);
+            FillRect(t, 5, 10, 2, 4, outline);
+            FillRect(t, 15, 10, 2, 4, outline);
 
-            if (style == 1)
+            if (jacketIndex == 1)
             {
-                FillRect(t, 5, 9, 2, 5, Lighten(jacket, 0.12f));
-                FillRect(t, 11, 9, 2, 5, Darken(jacket, 0.16f));
+                FillRect(t, 7, 10, 2, 5, Lighten(jacket, 0.13f));
+                FillRect(t, 13, 10, 2, 5, Darken(jacket, 0.16f));
             }
-            else if (style == 2)
+            else if (jacketIndex == 2)
             {
-                t.SetPixel(6, 12, Lighten(jacket, 0.2f));
-                t.SetPixel(11, 12, Lighten(jacket, 0.2f));
-                FillRect(t, 5, 13, 8, 1, Darken(jacket, 0.25f));
+                t.SetPixel(8, 13, Lighten(jacket, 0.22f));
+                t.SetPixel(13, 13, Lighten(jacket, 0.22f));
+                FillRect(t, 7, 14, 8, 1, Darken(jacket, 0.22f));
             }
-            else if (style == 5)
+            else if (jacketIndex == 5)
             {
-                FillRect(t, 5, 8, 8, 1, new Color32(70, 220, 226, 255));
-                t.SetPixel(6, 10, new Color32(70, 220, 226, 255));
-                t.SetPixel(11, 10, new Color32(70, 220, 226, 255));
+                FillRect(t, 7, 9, 8, 1, new Color32(70, 220, 226, 255));
+                t.SetPixel(8, 11, new Color32(70, 220, 226, 255));
+                t.SetPixel(13, 11, new Color32(70, 220, 226, 255));
             }
         }
 
         private void DrawHair(Texture2D t, Direction direction, Color32 hair, Color32 outline)
         {
-            FillRect(t, 4, 21, 10, 4, outline);
-            FillRect(t, 5, 21, 8, 3, hair);
+            FillEllipse(t, 11, 25, 8, 4, outline);
+            FillEllipse(t, 11, 25, 7, 3, hair);
 
-            if (hairStyleIndex == 0) FillRect(t, 5, 19, 3, 3, hair);
-            else if (hairStyleIndex == 1) { FillRect(t, 4, 20, 4, 4, hair); FillRect(t, 10, 22, 4, 3, hair); }
-            else if (hairStyleIndex == 2) { FillRect(t, 5, 20, 8, 3, hair); FillRect(t, 10, 23, 4, 2, hair); }
-            else if (hairStyleIndex == 3) { FillRect(t, 4, 19, 3, 5, hair); FillRect(t, 11, 21, 3, 3, Darken(hair, 0.18f)); }
-            else if (hairStyleIndex == 4) { FillRect(t, 4, 19, 4, 5, hair); FillRect(t, 10, 19, 4, 5, hair); }
-            else { FillRect(t, 4, 18, 3, 6, hair); FillRect(t, 11, 18, 3, 6, hair); }
+            if (hairStyleIndex == 0)
+            {
+                FillRect(t, 5, 23, 3, 3, hair);
+            }
+            else if (hairStyleIndex == 1)
+            {
+                FillRect(t, 4, 23, 4, 4, hair);
+                FillRect(t, 13, 25, 4, 3, hair);
+                t.SetPixel(7, 28, hair);
+                t.SetPixel(14, 28, hair);
+            }
+            else if (hairStyleIndex == 2)
+            {
+                FillRect(t, 7, 26, 8, 3, hair);
+                FillRect(t, 13, 27, 4, 2, hair);
+            }
+            else if (hairStyleIndex == 3)
+            {
+                FillRect(t, 4, 22, 3, 5, hair);
+                FillRect(t, 14, 24, 3, 3, Darken(hair, 0.18f));
+            }
+            else if (hairStyleIndex == 4)
+            {
+                FillEllipse(t, 6, 24, 4, 4, hair);
+                FillEllipse(t, 15, 24, 4, 4, hair);
+                FillEllipse(t, 10, 27, 4, 3, hair);
+            }
+            else
+            {
+                FillRect(t, 4, 21, 3, 7, hair);
+                FillRect(t, 15, 21, 3, 7, hair);
+            }
 
-            if (direction == Direction.Up) FillRect(t, 5, 18, 8, 5, hair);
+            if (direction == Direction.Up)
+            {
+                FillEllipse(t, 11, 23, 7, 6, hair);
+                FillRect(t, 6, 21, 10, 4, hair);
+            }
         }
 
         private static void DrawFace(Texture2D t, Direction direction, Color32 outline)
         {
             if (direction == Direction.Down)
             {
-                t.SetPixel(7, 19, outline);
-                t.SetPixel(11, 19, outline);
-                FillRect(t, 8, 16, 3, 1, new Color32(161, 96, 79, 255));
+                t.SetPixel(8, 22, outline);
+                t.SetPixel(13, 22, outline);
+                t.SetPixel(10, 19, new Color32(158, 91, 76, 255));
+                t.SetPixel(11, 19, new Color32(158, 91, 76, 255));
             }
-            else if (direction == Direction.Left) t.SetPixel(5, 19, outline);
-            else if (direction == Direction.Right) t.SetPixel(12, 19, outline);
+            else if (direction == Direction.Left)
+            {
+                t.SetPixel(6, 22, outline);
+            }
+            else if (direction == Direction.Right)
+            {
+                t.SetPixel(15, 22, outline);
+            }
         }
 
         private void DrawAccessory(Texture2D t, Direction direction, Color32 outline)
@@ -301,27 +356,54 @@ namespace Pythime
             if (accessoryIndex == 1)
             {
                 var bag = new Color32(92, 61, 40, 255);
-                if (direction == Direction.Up) { FillRect(t, 6, 9, 6, 5, outline); FillRect(t, 7, 10, 4, 3, bag); }
-                else FillRect(t, direction == Direction.Left ? 13 : 2, 10, 3, 5, bag);
+                if (direction == Direction.Up)
+                {
+                    FillRect(t, 8, 10, 6, 5, outline);
+                    FillRect(t, 9, 11, 4, 3, bag);
+                }
+                else
+                {
+                    FillRect(t, direction == Direction.Left ? 16 : 3, 11, 3, 5, outline);
+                    FillRect(t, direction == Direction.Left ? 17 : 4, 12, 2, 3, bag);
+                }
             }
             else if (accessoryIndex == 2)
             {
                 var cap = new Color32(165, 54, 54, 255);
-                FillRect(t, 4, 22, 10, 2, cap);
-                if (direction == Direction.Down) FillRect(t, 7, 21, 6, 2, cap);
+                FillRect(t, 5, 26, 12, 2, outline);
+                FillRect(t, 6, 27, 10, 1, cap);
+                if (direction == Direction.Down) FillRect(t, 10, 25, 7, 2, cap);
             }
             else if (accessoryIndex == 3)
             {
                 var phones = new Color32(55, 60, 69, 255);
-                FillRect(t, 3, 18, 2, 4, phones);
-                FillRect(t, 13, 18, 2, 4, phones);
+                FillRect(t, 4, 21, 2, 4, phones);
+                FillRect(t, 16, 21, 2, 4, phones);
             }
             else if (accessoryIndex == 4)
             {
                 var chrono = new Color32(66, 219, 230, 255);
-                if (direction == Direction.Up) FillRect(t, 7, 10, 4, 3, chrono);
-                else FillRect(t, direction == Direction.Left ? 13 : 3, 10, 2, 4, chrono);
+                if (direction == Direction.Up)
+                {
+                    FillRect(t, 9, 11, 4, 3, outline);
+                    FillRect(t, 10, 12, 2, 1, chrono);
+                }
+                else
+                {
+                    var x = direction == Direction.Left ? 16 : 4;
+                    FillRect(t, x, 11, 2, 4, outline);
+                    t.SetPixel(x + 1, 12, chrono);
+                    t.SetPixel(x + 1, 13, chrono);
+                }
             }
+        }
+
+        private Color32 ShoeColor()
+        {
+            if (shoesIndex == 0) return new Color32(231, 231, 225, 255);
+            if (shoesIndex == 1) return new Color32(34, 37, 43, 255);
+            if (shoesIndex == 2) return new Color32(91, 63, 43, 255);
+            return new Color32(180, 53, 52, 255);
         }
 
         private static int Wrap(int value, int length)
@@ -346,6 +428,22 @@ namespace Pythime
             for (var px = x; px < x + width; px++)
                 if (px >= 0 && py >= 0 && px < texture.width && py < texture.height)
                     texture.SetPixel(px, py, color);
+        }
+
+        private static void FillEllipse(Texture2D texture, int centerX, int centerY, int radiusX, int radiusY, Color32 color)
+        {
+            if (radiusX <= 0 || radiusY <= 0) return;
+            for (var y = centerY - radiusY; y <= centerY + radiusY; y++)
+            {
+                for (var x = centerX - radiusX; x <= centerX + radiusX; x++)
+                {
+                    var nx = (x - centerX) / (float)radiusX;
+                    var ny = (y - centerY) / (float)radiusY;
+                    if (nx * nx + ny * ny > 1f) continue;
+                    if (x >= 0 && y >= 0 && x < texture.width && y < texture.height)
+                        texture.SetPixel(x, y, color);
+                }
+            }
         }
 
         private static Color32 Darken(Color32 color, float amount)
